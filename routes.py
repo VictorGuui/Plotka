669dc8ae34b31a01137a7c6be6addf1bafab0ba6
imbard <nicota1234@gmail.com>
2023-08-20 05:36:12 -0300

    fix body from request

--- routes.py
+++ routes.py
@@ -3,6 +3,7 @@ from flask import request, jsonify, Blueprint
 from sqlalchemy.exc import SQLAlchemyError
 from datetime import datetime
 from db import connect_db
+from estados import message_handler
 
 routes = Blueprint('routes', __name__)
 
@@ -11,10 +12,13 @@ def sendMessage():
     try:
         body = request.get_json()
         content = body.get("content")
-        role = "PEDROCA"
+        role = body.get("role")
+
+        print(body)
 
         connection = connect_db()
         cursor = connection.cursor()
+
 
         query = "INSERT INTO messages (content, role, datetime) values (%s, %s, %s)"
         cursor.execute(query, (content, role, datetime.now()))
@@ -22,6 +26,9 @@ def sendMessage():
 
         cursor.close()
         connection.close()
+
+        result = message_handler(body)
+        
         return jsonify({"message": "Dados salvos com sucesso!"}), 201
     except Exception as e:
         return jsonify({"error": str(e)}), 500
